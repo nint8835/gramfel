7fb48beb8e4be6e892c02ae80eec01fc2462e363
Riley Flynn <riley@rileyflynn.me>
2023-12-11 17:46:30 -0330

    Keenan countdown

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,35 @@ export interface Env {
 
 export default {
     async scheduled(controller: ScheduledController, env: Env, ctx: ExecutionContext): Promise<void> {
-        await fetch(env.DISCORD_WEBHOOK, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                content: '<:harrhy:569924236353994782>',
-            }),
-        });
+        switch (controller.cron) {
+            case '0 17 * * *': {
+                await fetch(env.DISCORD_WEBHOOK, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        content: '<:harrhy:569924236353994782>',
+                    }),
+                });
+                break;
+            }
+            case '30 11 * * *': {
+                const targetDay = new Date(2024, 0, 2);
+                const today = new Date();
+                const diffDays = Math.ceil((targetDay.getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
+
+                await fetch(env.DISCORD_WEBHOOK, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        content: diffDays.toLocaleString(),
+                    }),
+                });
+                break;
+            }
+        }
     },
 };
