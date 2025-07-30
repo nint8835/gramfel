017f4ca53a8961457dd99662f3552f268a6681d8
Riley Flynn <riley@rileyflynn.me>
2025-07-30 17:34:07 -0230

    Revert "brackets plz (#8)"
    This reverts commit 1083453402c7a607deffb2f48212b0af50bce740.

--- src/index.ts
+++ src/index.ts
@@ -26,10 +26,6 @@ const responses: Record<string, MessageGenerator> = {
             'Set a new goal for this week. You are legally obligated to disclose whether you completed last weeks goal, and chat is permitted to shade you if the goal was not achieved.\n<@224890702218133505> <@742187091475169300> <@480415224164253707> <@163488287951028227>',
         channel: 'ok',
     }),
-    '30 19 * * *': () => ({
-        content: '<@163488287951028227> brackets pls',
-        channel: 'general',
-    }),
 };
 
 export default {
