b755156fdc78f0d6047fea6dfa3ffcbcfc1ab51b
ag <agaudon@proton.me>
2025-08-04 16:13:44 -0230

    Added grass touching reminder

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 export interface Env {
     DISCORD_WEBHOOK: string;
     OK_WEBHOOK_URL: string;
+    GRASS_TOUCHING_WEBHOOK: string;
 }
 
-type Channel = 'general' | 'ok';
+type Channel = 'general' | 'ok' | 'grass-touching';
 
 interface Message {
     content: string;
@@ -26,12 +27,27 @@ const responses: Record<string, MessageGenerator> = {
             'Set a new goal for this week. You are legally obligated to disclose whether you completed last weeks goal, and chat is permitted to shade you if the goal was not achieved.\n<@224890702218133505> <@742187091475169300> <@480415224164253707> <@163488287951028227>',
         channel: 'ok',
     }),
+    '15 18 * * *': () => ({
+        content: 'Grass touching <@489123999889227776>',
+        channel: 'grass-touching',
+    }),
 };
 
 export default {
     async scheduled(controller: ScheduledController, env: Env, ctx: ExecutionContext): Promise<void> {
         const message = responses[controller.cron]();
-        const webhookUrl = message.channel === 'ok' ? env.OK_WEBHOOK_URL : env.DISCORD_WEBHOOK;
+
+        const channelToWebhook: Record<Channel, string> = {
+            general: env.DISCORD_WEBHOOK,
+            ok: env.OK_WEBHOOK_URL,
+            'grass-touching': env.GRASS_TOUCHING_WEBHOOK,
+        };
+
+        const webhookUrl = channelToWebhook[message.channel];
+
+        if (!webhookUrl) {
+            throw new Error(`No webhook URL found for channel: ${message.channel}`);
+        }
 
         console.log(`Sending message to ${message.channel} channel: ${message.content}`);
 
